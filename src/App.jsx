b251502ccd999cc1ed819511947998a8aa4821c2
Alam Guardin <alamguardin@gmail.com>
2024-08-26 21:03:35 -0500

    feat: choose character

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
   const [turn, setTurn] = useState('x')
   const [winner, setWinner ] = useState(null)
   const [ranking, setRanking] = useState({x: 0, o: 0})
+  const [player, setPlayer] = useState(null)
 
   const updateRanking = () => {
     if (winner !== null) {
@@ -62,6 +63,10 @@ function App() {
     setBoard(new Array(9).fill(null))
   }
 
+  const handleChooseCharacter = (player) => {
+    setPlayer(player)
+  }
+
   useEffect(() => {
    checkWinner(board)
    updateRanking()
@@ -71,13 +76,13 @@ function App() {
     <div className='container'>
       <h1 className='heading'>Tic Tac Toe</h1>
       {/*Choose character popup*/}
-      <div className='choose__character'>
+      <div className={!player ? 'choose__character active' : 'choose__character'}>
         <h2 className='choose__character-title'>Choose your character</h2>
         <div className='choose__character-actions'>
-          <button className='choose__character-btn'>
+          <button className='choose__character-btn' onClick={() => handleChooseCharacter('x')}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path></svg>
           </button>
-          <button className='choose__character-btn'>
+          <button className='choose__character-btn' onClick={() => handleChooseCharacter('o')}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"></path></svg>
           </button>
         </div>
@@ -103,6 +108,9 @@ function App() {
         }
       </div>
       <button className='restart__btn' onClick={handleRestartBoard}>Restart Board</button>
+      { player &&
+        <p className='players'>User is "{player}", CPU is "{player === 'x' ? 'o' : 'x'}"</p>
+      }
     </div>
   )
 }
